b797633ec7536570bbb04d7c7519eebe3135b53e
TrainerAudi <audrictan13@gmail.com>
2023-10-20 00:06:19 -0400

    Milestone 1 (kinda) met

--- C292 Final - Fuzzy Killer/Assets/Scripts/PlayerMovement.cs
+++ C292 Final - Fuzzy Killer/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] int MAX_X_VELOCITY = 5;
+    [SerializeField] int MAX_Y_VELOCITY = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +16,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("w") || Input.GetKeyDown("up"))
         {
-            GetComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, 8);
+            if (GetComponent<Rigidbody2D>().velocity.y > MAX_Y_VELOCITY)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, MAX_Y_VELOCITY);
+            }
         }
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(5, 0);
+            if (GetComponent<Rigidbody2D>().velocity.x > MAX_X_VELOCITY)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(MAX_X_VELOCITY, GetComponent<Rigidbody2D>().velocity.y);
+            }
+        }
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(-5, 0);
+            if (GetComponent<Rigidbody2D>().velocity.x < 0- MAX_X_VELOCITY)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0-MAX_X_VELOCITY, GetComponent<Rigidbody2D>().velocity.y);
+            }
+        }
+        
     }
 }
